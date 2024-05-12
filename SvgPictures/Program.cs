b95b27722f2dd042f3bce0616ce6e47b35eadd62
James Coliz <jcoliz@outlook.com>
2024-05-11 20:34:01 -0700

    feat: Update to latest lib calling convention

--- SvgPictures/Program.cs
+++ SvgPictures/Program.cs
@@ -5,33 +5,30 @@ var pres = new Presentation();
 
 var shapes = pres.Slides[0].Shapes;
 
-var svg = SvgDocument.Open("cocktail-svgrepo-com.svg");
+var fileStream = File.OpenRead("cocktail-svgrepo-com.svg");
 
-var renderer = SvgRenderer.FromNull();
-var w = svg.Width.ToDeviceValue(renderer, UnitRenderingType.Horizontal, svg);
-var h = svg.Height.ToDeviceValue(renderer, UnitRenderingType.Vertical, svg);
-Console.WriteLine("{0} {1}", w, h);
+fileStream.Position = 0;
+shapes.AddPicture(fileStream);
 
-shapes.AddPictureSvg(svg,96,96);
-shapes.AddPictureSvg(svg,48,48);
+shapes.AddPicture(fileStream);
 var picture = shapes.GetByName<IPicture>("Picture 2");
 picture.X += 150;
 picture.Width = 96;
 picture.Height = 96;
 
-shapes.AddPictureSvg(svg,24,24);
+shapes.AddPicture(fileStream);
 picture = shapes.GetByName<IPicture>("Picture 3");
 picture.X += 300;
 picture.Width = 96;
 picture.Height = 96;
 
-shapes.AddPictureSvg(svg,12,12);
+shapes.AddPicture(fileStream);
 picture = shapes.GetByName<IPicture>("Picture 4");
 picture.X += 450;
 picture.Width = 96;
 picture.Height = 96;
 
-shapes.AddPictureSvg(svg,6,6);
+shapes.AddPicture(fileStream);
 picture = shapes.GetByName<IPicture>("Picture 5");
 picture.X += 600;
 picture.Width = 96;
