2e2519cf4d0b63bb8f2dfb774c863e697eaa4bdd
James Coliz <jcoliz@outlook.com>
2024-12-14 21:40:10 -0800

    feat: Adding .\Issue-649-AddPicture\ project

---
+++ Issue-649-AddPicture/Program.cs
@@ -0,0 +1,10 @@
+﻿//using DocumentFormat.OpenXml.Presentation;
+
+using ShapeCrawler;
+
+var pres = new Presentation("picture-adding.pptx");
+using var image = new FileStream("picture.png", FileMode.Open, FileAccess.Read);
+
+pres.Slides[0].Shapes.AddPicture(image);
+
+pres.SaveAs("picture-added.pptx");
