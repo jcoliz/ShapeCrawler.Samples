f35907894aed90348ddf9cc632f801ca203d40c3
James Coliz <jcoliz@outlook.com>
2024-12-14 22:19:21 -0800

    feat: Add many geometry presets

--- Shape.SetGeometry/Program.cs
+++ Shape.SetGeometry/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShapeCrawler;
+
+var pres = new Presentation();
+var shapes = pres.Slides[0].Shapes;
+
+shapes.AddRectangle(48, 96, 96*2, 96);
+var shape = shapes[^1];
+shape.GeometryType = Geometry.RoundRectangle;
+shape.CornerSize = 0.5m;
+
+shapes.AddRectangle(48, 96*3, 96*2, 96*4);
+shape = shapes[^1];
+shape.GeometryType = Geometry.Round2SameRectangle;
+shape.CornerSize = 0.25m;
+
+shapes.AddRectangle(96*2, 96, 96, 96);
+shape = shapes[^1];
+shape.GeometryType = Geometry.Star5;
+
+shapes.AddRectangle(96*3, 96, 96, 96);
+shape = shapes[^1];
+shape.GeometryType = Geometry.Ellipse;
+
+pres.SaveAs("out/Shape.SetGeometry.pptx");
