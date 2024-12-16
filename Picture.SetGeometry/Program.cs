7d4c4c28b150b9c45a8ce1a3fd62dc75d4b48516
James Coliz <jcoliz@outlook.com>
2024-12-15 21:03:08 -0800

    feat: Implement picture geometry

--- Picture.SetGeometry/Program.cs
+++ Picture.SetGeometry/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShapeCrawler;
+
+var pres = new Presentation();
+var shapes = pres.Slides[0].Shapes;
+
+using var stream = File.OpenRead("2x2.svg");
+shapes.AddPicture(stream);
+var pic = shapes[^1];
+pic.GeometryType = Geometry.TopCornersRoundedRectangle;
+pic.CornerSize = 25;
+
+pres.SaveAs("out/Picture.SetGeometry.pptx");
